0505c30eafcba67fe41a26e91850d24d39117946
nitesh aru <ide9892nitesh@gmail.com>
2023-01-25 17:59:16 +0530

    completed UI till more fragment and started working on fantasy sports prediction UI

---
+++ app/src/main/java/com/example/livecrickettvscores/Activities/Fragments/CareerFragment.java
@@ -0,0 +1,21 @@
+package com.example.livecrickettvscores.Activities.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.livecrickettvscores.R;
+
+public class CareerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_career, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/livecrickettvscores/Activities/PredictionsMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.livecrickettvscores.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.livecrickettvscores.R;
+
+public class PredictionsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_predictions_main);
+    }
+}
